4c2544fa2286ae69574a765b25e9ad93400699f8
Cedric Beau <cedric7693@hotmail.fr>
2020-06-13 12:21:15 +0200

    Refactror JS

--- js/todo.js
+++ js/todo.js
@@ -2,117 +2,117 @@
 
 /* --------------- Variables: Ready --------------- */
 
-let todoList = document.getElementById('todoList')
-const inputTask = document.getElementById('inputTask')
-const addTask = document.getElementById('addTask')
-const closeAlertBtn = document.getElementById('closeAlert')
-const resetBtn = document.getElementById('resetTodoList')
+let todoList = document.getElementById('todoList');
+let inputTask = document.getElementById('inputTask');
+let addTask = document.getElementById('addTask');
+let closeAlertBtn = document.getElementById('closeAlert');
+let resetBtn = document.getElementById('resetTodoList');
 
 /* --------------- Appel fonctions: Ready --------------- */
 
-getValues()
-labelForInput()
-storeCheck()
-addCheckmark()
-removeTasks()
+getValues();
+labelForInput();
+storeCheck();
+addCheckmark();
+removeTasks();
 
 /* --------------- Create Task: Ready --------------- */
 
 function createTask(){
 
-  let newTask, checkTask, labelTask, btnRemoveTask
-
   // LI
-  newTask = document.createElement('li')
-  newTask.classList.add('list__item')
+  let newTask = document.createElement('li');
+  newTask.classList.add('list__item');
 
   // CHECKBOX
-  checkTask = document.createElement('input')
-  checkTask.setAttribute('type', 'checkbox')
-  checkTask.classList.add('check__task')
+  let checkTask = document.createElement('input');
+  checkTask.type = 'checkbox';
+  checkTask.classList.add('check__task');
 
   // LABEL
-  labelTask = document.createElement('label')
-  labelTask.classList.add('item__task')
-  labelTask.innerHTML = '<span class="icon-checkmark2"></span>'+inputTask.value
+  let labelTask = document.createElement('label');
+  labelTask.classList.add('item__task');
+  labelTask.innerHTML = '<span class="icon-checkmark2"></span>'+inputTask.value;
 
   // BTN
-  btnRemoveTask = document.createElement('button')
-  btnRemoveTask.classList.add('remove__task')
-  btnRemoveTask.innerHTML = '<span class="icon-bin"></span>'
-  btnRemoveTask.addEventListener('click', removeTask, false)
+  let btnRemoveTask = document.createElement('button');
+  btnRemoveTask.classList.add('remove__task');
+  btnRemoveTask.innerHTML = '<span class="icon-bin"></span>';
+  btnRemoveTask.addEventListener('click', removeTask, false);
 
   // Append
-  newTask.appendChild(checkTask)
-  newTask.appendChild(labelTask)
-  newTask.appendChild(btnRemoveTask)
+  newTask.appendChild(checkTask);
+  newTask.appendChild(labelTask);
+  newTask.appendChild(btnRemoveTask);
 
   // Alerte
-  let alertBox = document.querySelector('.alert-danger')
-  // Supprime l'alerte si existe au moment de l'ajout de la nouvelle tâche
-  alertBox.style.display = 'none'
+  let alertBox = document.querySelector('.alert-danger');
+
   // Si champ vide => alerte
   if(inputTask.value === '') {
-    alertBox.style.display = 'flex'
+    alertBox.style.display = 'flex';
   } else {
     // sinon => ajoute tâche
-    todoList.appendChild(newTask)
+    todoList.appendChild(newTask);
   }
 
   // Associe checkbox et label
-  labelForInput()
+  labelForInput();
   // Style reset btn
-  styleBtnReset()
+  styleBtnReset();
   // Stock la liste localement
-  store()
+  store();
   // Stock l'état de la checkbox localement
-  storeCheck()
+  storeCheck();
 
   // Vide le champ une fois la tâche ajoutée
-  inputTask.value = ''
+  inputTask.value = '';
 }
 
 // Ajoute une tache en appuyant sur la touche 'Enter'
 inputTask.addEventListener('keydown', function(e){
   if(e.key === 'Enter') {
-    createTask()
+    createTask();
   }
 })
 
 // Ajoute une tache en cliquant sur le boutton 'Ajouter'
-addTask.addEventListener('click', createTask, false)
+addTask.addEventListener('click', function() {
+  createTask();
+})
 
 /* --------------- Associe le label à la checkbox: Ready --------------- */
 
 function labelForInput() {
 
-  let taskInputs = document.querySelectorAll('.check__task')
-  let taskLabels = document.querySelectorAll('.item__task')
+  let taskInputs = document.querySelectorAll('.check__task');
+  let taskLabels = document.querySelectorAll('.item__task');
 
   for(let i = 0; i<taskInputs.length; i++){
-    let taskInput = taskInputs[i]
-    let newId = 'task'+i
-    taskInput.setAttribute('id', newId)
+    let taskInput = taskInputs[i];
+    let newId = 'task'+i;
+    taskInput.setAttribute('id', newId);
   }
 
   for(let i = 0; i<taskLabels.length; i++){
-    let taskLabel = taskLabels[i]
-    let newId = 'task'+i
-    taskLabel.setAttribute('for', newId)
+    let taskLabel = taskLabels[i];
+    let newId = 'task'+i;
+    taskLabel.setAttribute('for', newId);
   }
 }
 
 /* --------------- Add checkmark: Ready --------------- */
 
 function addCheckmark() {
-  let taskInputs = document.querySelectorAll('.check__task')
+  let taskInputs = document.querySelectorAll('.check__task');
+
   for(let i = 0; i<taskInputs.length; i++){
-    let taskInput = taskInputs[i]
+    let taskInput = taskInputs[i];
+
     if(taskInput.checked === true) {
-      taskInput.nextSibling.innerHTML = '<span class="icon-checkmark"></span>'+taskInput.nextSibling.innerText
-    }
-    if(taskInput.checked === false) {
-      taskInput.nextSibling.innerHTML = '<span class="icon-checkmark2"></span>'+taskInput.nextSibling.innerText
+      taskInput.nextSibling.innerHTML = '<span class="icon-checkmark"></span>'+taskInput.nextSibling.innerText;
+    } else {
+      taskInput.nextSibling.innerHTML = '<span class="icon-checkmark2"></span>'+taskInput.nextSibling.innerText;
     }
   }
 }
@@ -122,17 +122,18 @@ function addCheckmark() {
 // Store checkbox values
 function storeCheck() {
 
-  let taskInputs = document.querySelectorAll('.check__task')
+  let taskInputs = document.querySelectorAll('.check__task');
 
   for(let i = 0; i<taskInputs.length; i++){
-    let taskInput = taskInputs[i]
+    let taskInput = taskInputs[i];
+
     taskInput.addEventListener('click', function(){
       if(taskInput.checked === true) {
-        localStorage.setItem('task'+i, taskInput.checked)
-        taskInput.nextSibling.innerHTML = '<span class="icon-checkmark"></span>'+taskInput.nextSibling.innerText
+        localStorage.setItem('task'+i, taskInput.checked);
+        taskInput.nextSibling.innerHTML = '<span class="icon-checkmark"></span>'+taskInput.nextSibling.innerText;
       } else {
-        localStorage.removeItem('task'+i, taskInput.checked)
-        taskInput.nextSibling.innerHTML = '<span class="icon-checkmark2"></span>'+taskInput.nextSibling.innerText
+        localStorage.removeItem('task'+i, taskInput.checked);
+        taskInput.nextSibling.innerHTML = '<span class="icon-checkmark2"></span>'+taskInput.nextSibling.innerText;
       }
     }, false)
   }
@@ -140,7 +141,7 @@ function storeCheck() {
 
 // Store List
 function store() {
-  window.localStorage.myTodoList = todoList.innerHTML
+  window.localStorage.myTodoList = todoList.innerHTML;
 
 }
 
@@ -148,23 +149,23 @@ function store() {
 function getValues() {
 
   // Stock la liste
-  let todoList = document.getElementById('todoList')
+  let todoList = document.getElementById('todoList');
   // Stock la clé du localStorage
-  let storedValues = window.localStorage.myTodoList
+  let storedValues = window.localStorage.myTodoList;
   // Si la clé n'existe pas
   if(!storedValues) {
     return
   } else {
     // Sinon affiche la valeur stockée
-    todoList.innerHTML = storedValues
+    todoList.innerHTML = storedValues;
   }
 
-  let taskInputs = document.querySelectorAll('.check__task')
+  let taskInputs = document.querySelectorAll('.check__task');
   for(let i = 0; i<taskInputs.length; i++){
     // Récupère la valeur de la checkbox
     let checked = localStorage.getItem('task'+i)
     // Applique la valeur de la checkbox
-    taskInputs[i].checked = checked
+    taskInputs[i].checked = checked;
    }
 }
 
@@ -174,16 +175,16 @@ function removeTask(){
 
   // Supprime le localStorage lié à la checkbox
   if(this.previousSibling.previousSibling.checked === true) {
-    let removeValue = this.previousSibling.previousSibling.id
-    localStorage.removeItem(removeValue)
+    let removeValue = this.previousSibling.previousSibling.id;
+    localStorage.removeItem(removeValue);
   }
 
   // Supprime la tâche
   this.parentNode.parentNode.removeChild(this.parentNode)
   // Style reset btn
-  styleBtnReset()
+  styleBtnReset();
   // Enregistre dans le localstorage
-  store()
+  store();
 }
 
 function removeTasks() {
@@ -199,11 +200,11 @@ styleBtnReset()
 function styleBtnReset() {
 
   if(todoList.childElementCount === 0) {
-    resetBtn.style.opacity = '.3'
-    resetBtn.style.pointerEvents = 'none'
+    resetBtn.style.opacity = '.3';
+    resetBtn.style.pointerEvents = 'none';
   } else {
-    resetBtn.style.opacity = '1'
-    resetBtn.style.pointerEvents = 'all'
+    resetBtn.style.opacity = '1';
+    resetBtn.style.pointerEvents = 'all';
   }
 }
 
@@ -213,17 +214,17 @@ function clearTodoList() {
 
   // Supprime toutes les tâches
   while(todoList.lastChild) {
-    todoList.removeChild(todoList.lastChild)
+    todoList.removeChild(todoList.lastChild);
   }
   // Clear localStorage
   localStorage.clear();
   // Style reset btn
-  styleBtnReset()
+  styleBtnReset();
 }
 resetBtn.addEventListener('click', clearTodoList, false)
 
 /* --------------- Close Alert: Ready --------------- */
 
 closeAlertBtn.addEventListener('click', function(){
-  this.parentNode.style.display = 'none'
+  this.parentNode.style.display = 'none';
 })
